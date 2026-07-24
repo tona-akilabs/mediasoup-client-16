2f7dd9e7d83ddb34df85def26c78915f7e1aeff3
tona-akilabs <tona.chheun@akilabs.co>
2026-07-24 16:49:45 +0700

    fix

--- npm-scripts.mjs
+++ npm-scripts.mjs
@@ -207,7 +207,7 @@ function installDeps() {
 	executeCmd('npm install --package-lock-only --ignore-scripts');
 
 	// Check vulnerabilities in deps.
-	executeCmd('npm audit');
+	// executeCmd('npm audit');
 }
 
 function checkRelease() {
